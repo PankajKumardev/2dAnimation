d180ca5e540e6c370f365f7f9de002d80281a83f
pankaj kumar <pankajamsali@gmail.com>
2025-06-21 00:52:35 +0530

    style: update scrollbar styles and clean up page component formatting

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-amber-700 text-2xl">Hi</h1>
+      <h1 className="text-amber-700 text-2xl " >Hi</h1>
+
     </div>
   );
 }
